cf873a28032eb59f862c8d0dd6ea70a3937e2ae2
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2024-12-17 23:55:42 +0500

    confirmation

--- src/screens/HomeScreen/BookingConfirm/BookingConfirmation.tsx
+++ src/screens/HomeScreen/BookingConfirm/BookingConfirmation.tsx
@@ -17,75 +17,99 @@ import PricingDetail from "./PricingDetail";
 import BookedCondition from "./BookedCondition";
 import CheckCondition from "./CheckCondition";
 import { navigate } from "../../../navigation/RootNavigation";
+import { showToast } from "../../../utils/toast";
 
-const BookingConfirmation = (props: any) => {
-  const data = [
-    { label: "Item 1", value: "1" },
-    { label: "Item 2", value: "2" },
-    { label: "Item 3", value: "3" },
-    { label: "Item 4", value: "4" },
-    { label: "Item 5", value: "5" },
-    { label: "Item 6", value: "6" },
-    { label: "Item 7", value: "7" },
-    { label: "Item 8", value: "8" },
-  ];
+const BookingConfirmation = ({ route }) => {
+  const startEndDates = route?.params?.startEndDates;
+  const item = route?.params?.item;
+  const selectedDates = route?.params?.selectedDates;
+  const daysInRange = route?.params?.daysInRange;
+  const card = route?.params?.card;
+
+  const totalPrice = item?.rentalPrice * daysInRange;
+
+  const [check, setCheck] = useState(false);
+  const [status, setStatus] = useState(false);
   return (
     <SafeAreaContainer safeArea={false}>
       <Header titleText="Renting Confirmation" centerImg={false} />
-      <ScrollView style={{ flex: 1, marginHorizontal: 20 }} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        style={{ flex: 1, marginHorizontal: 20 }}
+        showsVerticalScrollIndicator={false}
+      >
         <View>
           <View gap-10 row>
             <Image
-              source={IMAGES.truck}
+              source={{ uri: item?.Media?.carPicture?.[0]?.base64 }}
               style={{ width: 160, height: 110 }}
               resizeMode="stretch"
             />
             <View>
               <Typography textType="bold" size={theme.fontSize.large20}>
-                Ford Truck 2021
+                {item?.carName}
               </Typography>
               <Typography
                 size={theme.fontSize.extraSmall12}
+                numberOfLines={1}
                 color={theme.color.descColor}
               >
-                Torem ipsum dolor sit amet,{"\n"}consectetur
+                {item?.description}
               </Typography>
               <Typography
                 textType="bold"
                 size={theme.fontSize.large20}
                 color={theme.color.blue}
               >
-                AED 7,200/day
+                {`AED ${item?.rentalPrice ?? 0}/day`}
               </Typography>
             </View>
           </View>
 
           <View marginV-10>
-            <VechileStatusItoms />
+            <VechileStatusItoms item={item} />
           </View>
           <View marginV-10>
-            <BookedDate />
+            <BookedDate
+              daysInRange={daysInRange}
+              startEndDates={startEndDates}
+            />
           </View>
           <View marginV-10>
-            <PricingDetail />
+            <PricingDetail item={item} totalPrice={totalPrice} />
           </View>
-          <View marginV-10>  
-            <BookedCondition />
-          </View>
-          <View marginV-20>
-          <CheckCondition />
-          </View>
+          <View marginV-10>
+            {/* // TODO:: CHNAGE TO AGAINST USER ID */}
+            <BookedCondition id={item?.ID} />
           </View>
           <View marginV-20>
-            <Typography >Renting conditions must be agreed upon before proceeding to pay.</Typography>
+            <CheckCondition
+              setCheck={setCheck}
+              check={check}
+              status={status}
+              setStatus={setStatus}
+            />
+          </View>
+        </View>
+        <View marginV-20>
+          <Typography>
+            Renting conditions must be agreed upon before proceeding to pay.
+          </Typography>
         </View>
         <Button
-        label="Confirm and Pay"
-        backgroundColor={theme.color.primary}
-        borderRadius={30}
-        onPress={() => navigate(SCREENS.CONTRACT)}
-        style={{ height: 50, margin: 20,width:'50%',alignSelf:"center" }}
-      />
+          label="Confirm and Pay"
+          backgroundColor={theme.color.primary}
+          borderRadius={30}
+          onPress={() => {
+            if (status && check) {
+              navigate(SCREENS.CONTRACT, {
+                item: item,
+              });
+            } else {
+              showToast({ title: "Please confirm the terms to proceed" });
+            }
+          }}
+          style={{ height: 50, margin: 20, width: "50%", alignSelf: "center" }}
+        />
       </ScrollView>
     </SafeAreaContainer>
   );
