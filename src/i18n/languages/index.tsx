9f14d05c727cf51a999722ab5f9264b51ec1b763
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2024-12-26 23:49:14 +0500

    asas

---
+++ src/i18n/languages/index.tsx
@@ -0,0 +1,6 @@
+export {default as english} from './english.json';
+export {default as dutch} from './dutch.json';
+export {default as portuguese} from './portuguese.json';
+export {default as german} from './german.json';
+export {default as spanish} from './spanish.json';
+export {default as arabic} from './arabic.json';
