cd6f6397d1af4f557ab23bc3b75ed619b646a6ce
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2024-12-26 23:48:32 +0500

    adsd

--- src/components/atoms/OnBeardingBottomBtn.tsx
+++ src/components/atoms/OnBeardingBottomBtn.tsx
@@ -6,9 +6,12 @@ import { Pressable, TouchableOpacity } from "react-native";
 import { navigate } from "../../navigation/RootNavigation";
 import { useDispatch } from "react-redux";
 import { setLoggedIn } from "../../redux/slice/user";
+import { COMMON_TEXT } from "../../constants/screens";
+import { useTranslation } from "../../hooks/useTranslation";
 
 export const OnBeardingBottomBtn = (props: any) => {
-  const {onPress = ()=>{}} = props;
+  const { onPress = () => {} } = props;
+  const { t } = useTranslation();
   return (
     <View margin-30 row spread>
       <TouchableOpacity onPress={() => navigate(SCREENS.LOGIN)}>
@@ -19,11 +22,17 @@ export const OnBeardingBottomBtn = (props: any) => {
             marginTop: 10,
           }}
         >
-          <Typography color={theme.color.blue} textType="bold" size={theme.fontSize.large}>Skip</Typography>
+          <Typography
+            color={theme.color.blue}
+            textType="bold"
+            size={theme.fontSize.large}
+          >
+            {COMMON_TEXT.SKIP}
+          </Typography>
         </View>
       </TouchableOpacity>
       <Button
-        label="Next"
+        label={t(COMMON_TEXT.NEXT)}
         backgroundColor={theme.color.primary}
         onPress={onPress}
       />
